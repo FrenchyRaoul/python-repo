2c527f61301b5b3d14599b4934198d3e977f391f
nlespera@us.ibm.com <nlespera@us.ibm.com>
2016-02-17 13:59:23 -0500

    building up

--- scanner/main.py
+++ scanner/main.py
@@ -1,2 +1,30 @@
+
+
 def main():
+    while True:
+        barcode = input("Please scan a barcode")
+        # Is this code a command code (add, subtract, bottle, change db)
+        if barcode[0:6] == '$CODE$':
+            mode = barcode.split('$')[1]
+            # If yes- change mode
+        else:
+            pass
+        # If no- Check if code exists in database
+            # If code exists, add to data base
+            # If code does not exist, queue to be added to db
+
+def add(code):
+    # First, find out if this code is already 'identified'. If so, add +1 to quantity
+    # If not, add new item to db function, and then add +1 to quantity
     pass
+
+def remove(code):
+    pass
+
+def select_schema(code):
+    pass
+
+MODE= {'add': add,
+       'remove': remove,
+       'change': select_schema,
+       }
